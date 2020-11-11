4685e46be07b294f13266b0f8756556a36511c1c
Petikov Vladimir <vx7xv@inbox.ru>
2020-11-11 18:27:25 +0300

    Сделал верхний блок business-card и добавил к нему адаптив

--- index.php
+++ index.php
@@ -2,18 +2,28 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
+<!--          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">-->
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Мое портфолио</title>
     <link rel="stylesheet" href="/css/krox-pf.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;
+                                                                                      900&display=swap">
 </head>
 <body>
     <div class="container">
-        <span>Фрилансер</span>
-        <span>Владимир Петиков</span>
-        <p>Разрабатываю сайты для малого и среднего бизнеса. У меня можно заказать дизайн и верстку небольшого сайта или интернет-магазина</p>
-        <a href="#">Бесплатная консультация</a>
+        <div class="business-card">
+            <div class="info">
+                <span class="profession">Фрилансер</span>
+                <span class="name">Владимир Петиков</span>
+                <p>Разрабатываю сайты для малого и среднего бизнеса.
+                    У меня можно заказать дизайн и верстку небольшого сайта или интернет-магазина</p>
+                <a href="#">Бесплатная&nbsp консультация</a>
+            </div>
+            <div class="img-card"></div>
+        </div>
+
+
     </div>
 </body>
 </html>
