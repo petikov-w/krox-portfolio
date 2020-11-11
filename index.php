48d80710e495ca356e683c20261c6c2a6db6452d
Petikov Vladimir <vx7xv@inbox.ru>
2020-11-11 15:20:21 +0300

    Создан index.php и .gitignore. Начало работы над проектом - 11.11.2020  15:00

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Мое портфолио</title>
+    <link rel="stylesheet" href="/css/krox-pf.css">
+</head>
+<body>
+    <div class="container">
+        <span>Фрилансер</span>
+        <span>Владимир Петиков</span>
+        <p>Разрабатываю сайты для малого и среднего бизнеса. У меня можно заказать дизайн и верстку небольшого сайта или интернет-магазина</p>
+        <a href="#">Бесплатная консультация</a>
+    </div>
+</body>
+</html>
